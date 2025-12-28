7e06203d93cccb8e9c529d5fdecf12f1f233ca4d
MatthiasProbst <matth.probst@gmail.com>
2025-12-28 09:47:50 +0100

    narrowing the dependencies

---
+++ build.bat
@@ -0,0 +1,6 @@
+pip install --upgrade build
+pip install twine==6.0.1
+pip install pkginfo==1.12.0
+python -m build
+python -m twine upload dist/*
+@REM python -m twine upload --repository testpypi dist/*
